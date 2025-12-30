0b986410b4fe849faecb7cc1fce45228b025ef93
Thomas Kalka <thomas.kalka@gmail.com>
2025-12-30 12:25:29 +0100

    cleanup

--- debug-step2.js
+++
@@ -1,6 +0,0 @@
-import { parseTutorial } from './packages/parser/src/parse-tutorial.js';
-import fs from 'fs';
-
-const markdown = fs.readFileSync('./test/snapshots/silly-eyes/repo/de-DE/step_2.md', 'utf8');
-const result = await parseTutorial(markdown);
-console.log(result.html);
--- pretty.js
+++
@@ -1,3 +0,0 @@
-import { readFileSync } from 'fs';
-const html = readFileSync('debug-only.html', 'utf-8');
-console.log(html.replace(/></g, '>\n<'));
